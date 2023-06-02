6e15869bd65ac48ea3eebc1dbd0e25c999bb2698
reda108 <redabouougri12@gmail.com>
2023-06-03 00:16:47 +0100

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:8050',
+      target: 'restaurantlocalisationback-production.up.railway.app',
       changeOrigin: true,
     })
   );
